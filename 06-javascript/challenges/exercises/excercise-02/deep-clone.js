58cc9c200d5eb905338a4c89fa5712775551749d
vivekgonuguntla <149650228+vivekgonuguntla@users.noreply.github.com>
2025-12-30 13:31:07 +0530

    Refactor deepClone function for brevity

--- 06-javascript/challenges/exercises/excercise-02/deep-clone.js
+++ 06-javascript/challenges/exercises/excercise-02/deep-clone.js
@@ -1,21 +1,18 @@
-let original = {
-    name: 'John',
-    address: {
-        city: 'New York',
-        zip: '10001'
-    },
-    hobbies: ['reading', 'gaming']
+const original = {
+  name: 'John',
+  address: {
+    city: 'New York',
+    zip: '10001'
+  },
+  hobbies: ['reading', 'gaming']
 };
 
-function deepClone(obj) {
-    // Your code here.......
-    return structuredClone(obj);
-    
-}
+const deepClone = (obj) => structuredClone(obj);
 
-let cloned = deepClone(original);
+const cloned = deepClone(original);
+
 cloned.address.city = 'Boston';
 cloned.hobbies.push('swimming');
 
-console.log(original.address.city);  // Should still be 'New York'
-console.log(original.hobbies);       // Should still be ['reading', 'gaming']
+console.log(`Original city: ${original.address.city}`); 
+console.log(`Original hobbies: ${original.hobbies.join(', ')}`);
