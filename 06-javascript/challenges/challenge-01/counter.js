7d2a942427c84b40b42d625b59eb3d1cb608af3e
vivekgonuguntla <149650228+vivekgonuguntla@users.noreply.github.com>
2025-12-30 12:47:19 +0530

    Update counter.js

--- 06-javascript/challenges/challenge-01/counter.js
+++ 06-javascript/challenges/challenge-01/counter.js
@@ -4,49 +4,44 @@ const decrementBtn = document.getElementById('decrement');
 const resetBtn = document.getElementById('reset');
 const stepBtns = document.querySelectorAll('.step-btn');
 
-
 let count = 0;
 let step = 1;
 
+const updateDisplay = () => {
+  countDisplay.textContent = `${count}`;
 
-function updateDisplay() {
-    countDisplay.textContent = count;
-
-    if (count > 0) {
-        countDisplay.style.color = 'green';
-    } else if (count < 0) {
-        countDisplay.style.color = 'red';
-    } else {
-        countDisplay.style.color = 'black';
-    }
-}
+  if (count > 0) {
+    countDisplay.style.color = 'green';
+  } else if (count < 0) {
+    countDisplay.style.color = 'red';
+  } else {
+    countDisplay.style.color = 'black';
+  }
+};
 
 incrementBtn.addEventListener('click', () => {
-    count += step;
-    updateDisplay();
+  count += step;
+  updateDisplay();
 });
-
 
 decrementBtn.addEventListener('click', () => {
-    count -= step;
+  count -= step;
 
-    if (count < 0) count = 0; 
+  if (count < 0) count = 0;
 
-    updateDisplay();
+  updateDisplay();
 });
 
-
 resetBtn.addEventListener('click', () => {
-    count = 0;
-    updateDisplay();
+  count = 0;
+  updateDisplay();
 });
 
 stepBtns.forEach(button => {
   button.addEventListener('click', () => {
-    step = Number(button.dataset.step);
-    stepBtns.forEach(btn => btn.classList.remove('active'));    
+    step = Number(`${button.dataset.step}`);
+
+    stepBtns.forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
   });
 });
-
-
