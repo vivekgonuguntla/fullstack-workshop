569c7406b6f7e3408c1bab7e35366d06484ddc15
vivekgonuguntla <149650228+vivekgonuguntla@users.noreply.github.com>
2025-12-30 13:30:12 +0530

    Refactor variable swap and update console logs

--- 06-javascript/challenges/exercises/excercise-02/Variable-Swap.js
+++ 06-javascript/challenges/exercises/excercise-02/Variable-Swap.js
@@ -1,10 +1,9 @@
 let x = 5;
 let y = 10;
 
-x  = x + y;
+x = x + y;
 y = x - y;
 x = x - y;
 
-console.log('value of x = ',x); // Should be 10
-
-console.log('value of y = ',y); // Should be 5
+console.log(`value of x = ${x}`); // 10
+console.log(`value of y = ${y}`); // 5
